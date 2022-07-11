a26120b4149af106f7eb58978dca54d47c13433d
lazywon <jinjooisme@gmail.com>
2022-07-11 22:42:51 +0900

    Stub을 사용하여 DI 코드 테스트

---
+++ src/mock/product/product_service.js
@@ -0,0 +1,14 @@
+class ProductService {
+  constructor(productClient) {
+    //DI 의존성 주입
+    this.productClient = productClient;
+  }
+
+  fetchAvailableItems() {
+    return this.productClient
+      .fetchItems()
+      .then((items) => items.filter((item) => item.available));
+  }
+}
+
+module.exports = ProductService;
